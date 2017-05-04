fa131ed133642f00cfcc053d3351f8e91a80d292
Krisztian Nagy <chrisakakay@gmail.com>
2017-05-04 13:32:26 +0200

    base rule config

---
+++ index.js
@@ -0,0 +1,66 @@
+/**
+ * @fileoverview Restricts underscore everywhere
+ * @author Krisztian Nagy
+ */
+
+'use strict';
+
+module.exports = {
+    rules: {
+        'no-underscore': {
+            meta: {
+                docs: {
+                    description: 'Restricts underscore everywhere',
+                    category: '',
+                    recommended: false
+                },
+                schema: [{
+                    type: 'object',
+                    properties: {
+                        allowConstants: {
+                            type: 'boolean'
+                        },
+                        allowLeadingUnderscores: {
+                            type: 'boolean'
+                        }
+                    },
+                    additionalProperties: false
+                }]
+            },
+
+            create: (context) => {
+                const options = context.options[0] || {};
+
+                function hasUnderscore(text) {
+                    return text.indexOf('_') > -1;
+                }
+
+                function isConstant(text) {
+                    return text === text.toUpperCase();
+                }
+
+                function report(node) {
+                    context.report({
+                        node,
+                        message: `Underscore found in '${node.name}'`
+                    });
+                }
+
+                return {
+                    Identifier: (node) => {
+                        let name = node.name;
+
+                        if (name === '_') return;
+                        if (options.allowLeadingUnderscores) {
+                            name = name.replace(/^_+/g, '');
+                        }
+                        if (options.allowConstants && isConstant(name)) return;
+                        if (!hasUnderscore(name)) return;
+
+                        report(node);
+                    }
+                }
+            }
+        }
+    }
+};
---
+++ index.test.js
@@ -0,0 +1,80 @@
+'use strict';
+
+const rule = require('./index').rules['no-underscore'];
+const RuleTester = require('eslint').RuleTester;
+
+RuleTester.setDefaultConfig({
+    parserOptions: {
+        ecmaVersion: 6,
+        sourceType: 'module'
+    }
+});
+
+const ruleTester = new RuleTester();
+
+ruleTester.run('no-underscore', rule, {
+    valid: [
+        'var aName = 3;',
+        '_.map([1,], function(n) { return n; });',
+        {
+            code: 'var CONST_NAME = 3;',
+            options: [{ allowConstants: true }]
+        },
+        {
+            code: 'var _a = 3;',
+            options: [{ allowLeadingUnderscores: true }]
+        }
+    ],
+    invalid: [
+        {
+            code: 'var a_name = 3;',
+            errors: [{
+                message: `Underscore found in 'a_name'`,
+                type: 'Identifier'
+            }]
+        },
+        {
+            code: 'var CONST_NAME = 3;',
+            errors: [{
+                message: `Underscore found in 'CONST_NAME'`,
+                type: 'Identifier'
+            }]
+        },
+        {
+            code: 'var _a = 3;',
+            errors: [{
+                message: `Underscore found in '_a'`,
+                type: 'Identifier'
+            }]
+        },
+        {
+            code: 'var _a_name = 3;',
+            options: [{ allowLeadingUnderscores: true }],
+            errors: [{
+                message: `Underscore found in '_a_name'`,
+                type: 'Identifier'
+            }]
+        },
+        {
+            code: 'var a = { a_name: 3 };',
+            errors: [{
+                message: `Underscore found in 'a_name'`,
+                type: 'Identifier'
+            }]
+        },
+        {
+            code: 'var a = {}; a.a_name = "a"',
+            errors: [{
+                message: `Underscore found in 'a_name'`,
+                type: 'Identifier'
+            }]
+        },
+        {
+            code: 'function a(_a) { return 1; }',
+            errors: [{
+                message: `Underscore found in '_a'`,
+                type: 'Identifier'
+            }]
+        },
+    ]
+});
